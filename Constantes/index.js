00319008c22ff4d746fdad31272e7ae4d586316b
nomytalo <email>
2021-11-02 02:49:10 -0300

    Alguns conceitos, e outros feito no console do navegador

---
+++ Constantes/index.js
@@ -0,0 +1,5 @@
+const FIRST_NAME = 'Ytalo Cruz'
+
+// FIRST_NAME = 'Erik'  //não pode declarar. 
+
+console.log(FIRST_NAME)
---
+++ Variáveis/indexx.js
@@ -0,0 +1,18 @@
+//Hoisting
+let numberOne;
+
+numberOne = 1;
+
+console.log(numberOne + 2);
+
+//
+
+var firstName = 'João';
+var lastName = 'Souza';
+
+if(firstName === 'João'){
+    var firstName = 'Pedro';
+    let lastName = 'Silva';
+}
+
+console.log(firstName, lastName)
