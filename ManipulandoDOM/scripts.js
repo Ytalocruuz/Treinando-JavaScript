6337a1fb7b8632ba550eb34ee18a9c4378079910
nomytalo <email>
2021-11-09 02:49:36 -0300

    Modulos e Debug

--- ManipulandoDOM/scripts.js
+++ ManipulandoDOM/scripts.js
@@ -27,4 +27,5 @@ const body = document.getElementsByTagName('body')[0];
 const footer = document.getElementsByTagName('footer')[0];
 
 
+
 button.addEventListener('click', changeMode);
---
+++ Modulo/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Ytalo", 19))
+console.log(mostraCidade("Ytalo", "Manaus-AM"))
+console.log(mostraHobby("sdsddsd", "Jogos"))
