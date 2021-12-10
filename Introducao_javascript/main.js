3eaf3f0502bec969c9cc3111279be068b5e0d86f
nomytalo <email>
2021-12-10 02:04:08 -0300

    Introducao ao javascript

---
+++ Introducao_javascript/main.js
@@ -0,0 +1,38 @@
+// const  nome = "Ytalo Cruz";
+// const idade = 20
+// const idade2 = 10
+
+// alert(idade + idade2); 
+
+const lista = [ "moças","pera","laranja"]
+lista.push("uva")
+console.log(lista.toString())
+console.log(lista.join("-"))
+
+// const fruta = {nome: "maça", ccr:"azul"}
+// console.log(fruta.nome)
+
+// const frutas = [{nome: "maça", ccr:"azul"},{nome: "uva", ccr:"roxa"}]
+// alert(frutas[1].nome)
+
+// const idade = prompt("Qual sua idade")
+
+// if(idade >- 18){
+//     alert("maior de idade")
+// }else{
+//     alert("Menor de idade")
+// }
+
+
+// const count = 0;
+
+// while (count <= 5){
+//     console.log(count)
+//     count ++
+// }
+
+// const count;
+// for(count = 0; count <= 5; count++){
+//     alert(count);
+// }
+
